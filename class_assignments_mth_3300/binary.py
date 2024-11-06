86f72c09b6f97cde923c0d20e156d8e8cb51e614
ajordonez <ajordonez179@gmail.com>
2024-11-06 00:00:15 -0500

    Adding binary.py to the repo

---
+++ class_assignments_mth_3300/binary.py
@@ -0,0 +1,27 @@
+#******************************************************************************
+# binary.py
+#******************************************************************************
+# Name: 
+#******************************************************************************
+# Collaborators/outside sources used 
+#(IMPORTANT! Write "NONE" if none were used):
+#
+#
+#
+# Reminder: you are to write your own code.
+#******************************************************************************
+# Overall notes (not to replace inline comments):
+#
+#
+
+# The two lines below set the random seed for testing on Gradescope. Comment out these lines to test your code on Spyder.
+#####################
+seed = input() # comment out to test on Spyder, but leave for Gradescope submission
+random.seed(seed) # comment out to test on Spyder, but leave for Gradescope submission
+#####################
+
+# YOUR CODE SHOULD START BELOW. THE LINES ABOVE NEED TO BE BEFORE YOUR CODE! #
+
+
+
+
