d5ff12213270b33f3b94d034b4d56836d8eba80f
ajordonez <ajordonez179@gmail.com>
2024-12-18 22:18:38 -0500

    Added more to Motorcycle_Regression_Model.R

--- RStudio/Motorcycle_Regression_Model.R
+++ RStudio/Motorcycle_Regression_Model.R
@@ -28,7 +28,7 @@ motor_data <- motor_data %>%
 
 #Making a logistic regression analysis for if there is a Driver fatality or not
 
-mod1 <- glm(FATALITY ~ BOROUGH+VEHICLE_MAKE+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS, data = motor_data, family = "binomial")
+mod1 <- glm(FATALITY ~ BOROUGH+VEHICLE_MAKE+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS+PERSON_AGE, data = motor_data, family = "binomial")
 summary(mod1)
 
 
@@ -39,7 +39,7 @@ library(car)
 vif(mod1)
 
 #Lets try the mod1 again with only the significant or close to significant factors p < 0.1
-mod1 <- glm(FATALITY ~ VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS, data = motor_data, family = "binomial")
+mod1 <- glm(FATALITY ~ VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS+PERSON_AGE, data = motor_data, family = "binomial")
 summary(mod1)
 
 null_mod1 <- glm(FATALITY ~ 1, data = motor_data, family = "binomial")
@@ -52,13 +52,13 @@ anova(null_mod1, mod1, test = "Chisq")
 
 #Making a logistic regression analysis for if there is an injury or fatality for the driver of the motorcycle
 
-mod2 <- glm(INJURY_OR_FATALITY ~ BOROUGH+VEHICLE_MAKE+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS, data = motor_data, family = "binomial")
+mod2 <- glm(INJURY_OR_FATALITY ~ BOROUGH+VEHICLE_MAKE+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS+PERSON_AGE, data = motor_data, family = "binomial")
 summary(mod2)
 
 #Interesting how the results for the INJURY_OR_FATALITY is different than that of just Fatalities, Manhattan seems to be a significant factor
 
 
-mod2 <- glm(INJURY_OR_FATALITY ~ BOROUGH+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS, data = motor_data, family = "binomial")
+mod2 <- glm(INJURY_OR_FATALITY ~ BOROUGH+VEHICLE_TYPE+PERSON_SEX+DRIVER_LICENSE_STATUS+PERSON_AGE, data = motor_data, family = "binomial")
 summary(mod2)
 
 null_mod2 <- glm(INJURY_OR_FATALITY ~ 1, data = motor_data, family = "binomial")
@@ -66,14 +66,39 @@ anova(null_mod2, mod2, test = "Chisq")
 
 
 
+#We will make a graphic that shows the injuries and fatalities per 100,000 residents
+#per Borough. We have to make a separate data frame and then mutate onto motor_data
+#to do that 
+borough_pop <- data.frame(
+  BOROUGH = c("BRONX", "BROOKLYN", "MANHATTAN", "QUEENS", "STATEN ISLAND"),
+  POPULATION = c(1379946, 2590516, 1596273, 2278029, 491133)
+)
+
+borough_pop$BOROUGH <- as.factor(borough_pop$BOROUGH)
+motor_data <- merge(motor_data, borough_pop, by = "BOROUGH")
+
+injury_or_fatality_summary <- motor_data %>%
+  group_by(BOROUGH) %>% summarise(INJURY_OR_FATALITY_COUNT = sum(INJURY_OR_FATALITY),
+                                  POPULATION = first(POPULATION)) %>%
+  mutate(RATE_PER_100K = (INJURY_OR_FATALITY_COUNT / POPULATION) * 100000)
+
+ggplot(injury_or_fatality_summary, aes(x = BOROUGH, y = RATE_PER_100K, fill = BOROUGH)) +
+  geom_bar(stat = "identity") + labs(title = "Injuries or Fatalities per 100,000 Residents by Borough",
+                                     x = "Borough", y = "Rate per 100,000 Residents") + theme_minimal() + theme(legend.position = "none")
 
 
 
+#We will make now a graph that shows the relationship between age and the likelihood of
+#a injury or fatality occurring. We would need to first do something very similar to what we did
+#for the borough graph, adding a new column to motor_data.
+age_summary <- motor_data %>%
+  group_by(PERSON_AGE) %>%  
+  summarise(TOTAL_CASES = n(), INJURY_OR_FATALITY_COUNT = sum(INJURY_OR_FATALITY)) %>%
+  mutate(PROBABILITY_PER_AGE = INJURY_OR_FATALITY_COUNT / TOTAL_CASES)  
 
-
-
-
-
+ggplot(age_summary, aes(x = PERSON_AGE, y = PROBABILITY_PER_AGE)) +
+  geom_point(color = "red", size = 2) +  
+  labs(title = "Likelihood of Injury or Fatality by Age", x = "Age", y = "Likelihood of Injury or Fatality") + theme_minimal()
 
 
 
