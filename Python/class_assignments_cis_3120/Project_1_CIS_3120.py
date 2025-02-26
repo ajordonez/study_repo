8f830bb2e5f920540f95c7d376d25746fcbecd17
ajordonez <ajordonez179@gmail.com>
2025-02-26 10:19:09 -0500

    Added comments to Project 1

--- Python/class_assignments_cis_3120/Project_1_CIS_3120.py
+++ Python/class_assignments_cis_3120/Project_1_CIS_3120.py
@@ -1,11 +1,19 @@
 # Project 1 for CIS 3120
 # Collaborators: Alejandro Ordonez, Wesley Fann
 
+
+#This function will be in the main function to first introduce what the program does
 def introduction():
     print("This program will be taking the averages of 3 integer values and returning the average.")
     print("After returning the average, it will compare each value to the average.")
     print()
+
+
+#Master list to store all the values of the 
 master_list = []
+
+
+#This function will be called in the main function to compare the average to the a b and c integer variables
 def compare_to_avg(a,b,c,avg):
     temp_list = [a,b,c]
     for i in range(3):
@@ -16,8 +24,10 @@ def compare_to_avg(a,b,c,avg):
         else: 
             print(f"The number {temp_list[i]} is less than the average")
 
+#Finds the average of a b and c integer variables in main func
 def find_average(a,b,c):
     return (a+b+c)/3
+
 def main():
     introduction()
 
@@ -25,7 +35,7 @@ def main():
     b = int(input("Enter the second integer: "))
     c = int(input("Enter the third integer: "))
     avg = find_average(a,b,c)
-
+    print(f"The average of the numbers is {avg:.4f}")
     compare_to_avg(a,b,c,avg)
 
 
