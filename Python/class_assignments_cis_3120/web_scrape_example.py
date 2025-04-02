cfc8bb50bd1bba07a7c9362bd0d873cb8d58e869
ajordonez <ajordonez179@gmail.com>
2025-04-02 18:08:19 -0400

    Editted web scrape example

--- Python/class_assignments_cis_3120/web_scrape_example.py
+++ Python/class_assignments_cis_3120/web_scrape_example.py
@@ -1,4 +1,14 @@
 import webbrowser
-webbrowser.open("www.baruch.cuny.edu")
+
+# This line of code will take a google maps request and will send you to the url.
+google = input("Enter a value to search via Google Maps: ")
+webbrowser.open_new_tab('http://www.google.com/maps?btnG=1&q=%s' % google)
+
+
+
+
+#webbrowser.open_new_tab("https://www.brooklyn.cuny.edu/")
+
+
 #if not successful, replace the above line with the following
 #webbrowser.open(“https://www.baruch.cuny.edu/”)
